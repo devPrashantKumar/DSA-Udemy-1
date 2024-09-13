25508f7727720dbd74eee548af6fa7011e353174
Prashant Kumar <2018187@iiitdmj.ac.in>
2024-09-13 13:04:20 +0530

    arrays

---
+++ 2-Arrays/TwoDArray.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] arr1 = new int[2][2];
+        System.out.println(Arrays.deepToString(arr1));
+
+        // this code is fine 
+        int[][] arr2;
+        arr2 = new int[2][];
+        arr2[0] = new int[2];
+        arr2[1] = new int[5];
+        System.out.println(Arrays.deepToString(arr2));
+
+    }
+}

6310444727fce41bf90bf664ece7ec8c4026480b
Prashant Kumar <2018187@iiitdmj.ac.in>
2024-09-14 16:04:01 +0530

    ArrayProjectUsingStreams

--- 2-Arrays/ArrayProject1/ArrayProject1UsingStreams.java
+++ 2-Arrays/ArrayProject1/ArrayProject1UsingStreams.java
@@ -16,8 +16,11 @@ public class ArrayProject1UsingStreams {
 
         Double averageTemp = Arrays.stream(dayTemp).mapToDouble(Float::doubleValue).average().orElse(0.0);
         /// Float averageTemp1 = Arrays.stream(dayTemp).average().orElse(0.0);
+        Double numDays = (double) (Arrays.stream(dayTemp).filter(x-> x>averageTemp).count());
 
         System.out.println("Average Temprature : "+averageTemp);
+        System.out.println("Days above Average Temprature : "+numDays);
+
         
     }
 }

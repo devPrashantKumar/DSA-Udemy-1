97bc264f8d9af72852759641c7a1884b9404f31d
Prashant Kumar <2018187@iiitdmj.ac.in>
2024-10-14 21:02:43 +0530

    binary heap added

--- BinaryHeap/BinaryHeapClass.java
+++ BinaryHeap/BinaryHeapClass.java
@@ -5,75 +5,69 @@ public class BinaryHeapClass {
     private int sizeOfTree;
 
     public BinaryHeapClass(int size) {
-        this.arr = new int[size+1];
-        this.sizeOfTree=0;
+        this.arr = new int[size + 1];
+        this.sizeOfTree = 0;
     }
 
-    public boolean isEmpty(){
-        return sizeOfTree==0;
+    public boolean isEmpty() {
+        return sizeOfTree == 0;
     }
 
-    public Integer peek(){
-        if(isEmpty()) return null;
+    public Integer peek() {
+        if (isEmpty())
+            return null;
         return arr[1];
     }
 
-    public int sizeOfBH(){
+    public int sizeOfBH() {
         return sizeOfTree;
     }
 
-    public void printLevelOrderTraversal(){
-        for(int i=1;i<=sizeOfTree;i++){
-            System.out.print(arr[i]+" ");
+    public void printLevelOrderTraversal() {
+        for (int i = 1; i <= sizeOfTree; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    public void printPreOrderTraversal(int index){
-        if(index>sizeOfTree) return;
-        System.out.print(arr[index]+" ");
-        printPreOrderTraversal(2*index);
-        printPreOrderTraversal(2*index+1);
+    public void printPreOrderTraversal(int index) {
+        if (index > sizeOfTree)
+            return;
+        System.out.print(arr[index] + " ");
+        printPreOrderTraversal(2 * index);
+        printPreOrderTraversal(2 * index + 1);
     }
 
-    public void printPostOrderTraversal(int index){
-        if(index>sizeOfTree) return;
-        printPostOrderTraversal(2*index);
-        printPostOrderTraversal(2*index+1);
-        System.out.print(arr[index]+" ");
+    public void printPostOrderTraversal(int index) {
+        if (index > sizeOfTree)
+            return;
+        printPostOrderTraversal(2 * index);
+        printPostOrderTraversal(2 * index + 1);
+        System.out.print(arr[index] + " ");
     }
 
-    public void printInOrderTraversal(int index){
-        if(index>sizeOfTree) return;
-        printInOrderTraversal(2*index);
-        System.out.print(arr[index]+" ");
-        printInOrderTraversal(2*index+1);
+    public void printInOrderTraversal(int index) {
+        if (index > sizeOfTree)
+            return;
+        printInOrderTraversal(2 * index);
+        System.out.print(arr[index] + " ");
+        printInOrderTraversal(2 * index + 1);
     }
 
-    public void heapify(int index, String heapType){
-        if(index<=1) return;
-        int parent = index/2;
-        if(heapType.equals("minHeap")){
-            if(arr[parent]>arr[index]){
-                int temp = arr[parent];
-                arr[parent]=arr[index];
-                arr[index]=temp;
-                heapify(parent, heapType);
-            }
-        }
-        
-        if(heapType.equals("maxHeap")){
-            if(arr[parent]<arr[index]){
-                int temp = arr[parent];
-                arr[parent]=arr[index];
-                arr[index]=temp;
-                heapify(parent, heapType);
-            }
+    public void heapify(int index) {
+        if (index <= 1)
+            return;
+        int parent = index / 2;
+        if (arr[parent] > arr[index]) {
+            int temp = arr[parent];
+            arr[parent] = arr[index];
+            arr[index] = temp;
+            heapify(parent);
         }
     }
 
-    public void insertElementInHeap(int data,String heapType){
-        arr[++sizeOfTree]=data;
-        heapify(sizeOfTree, heapType);
+    public void insertElementInHeap(int data) {
+        arr[++sizeOfTree] = data;
+        heapify(sizeOfTree);
     }
 }
--- BinaryHeap/BinaryHeapClassTest.java
+++ BinaryHeap/BinaryHeapClassTest.java
@@ -3,10 +3,10 @@ package BinaryHeap;
 public class BinaryHeapClassTest {
     public static void main(String[] args) {
         BinaryHeapClass bh = new BinaryHeapClass(20);
-        bh.insertElementInHeap(25,"minHeap");
-        bh.insertElementInHeap(14, "minHeap");
-        bh.insertElementInHeap(3, "minHeap");
-        bh.insertElementInHeap(18, "minHeap");
+        bh.insertElementInHeap(25);
+        bh.insertElementInHeap(14);
+        bh.insertElementInHeap(3);
+        bh.insertElementInHeap(18);
         bh.printLevelOrderTraversal();
     }
     

068bc8da438fd20a94d43aa11bae68237e660eec
Prashant Kumar <77582926+devPrashantKumar@users.noreply.github.com>
2024-10-14 10:57:53 +0530

    Create BinarySearchTreeNode.java

---
+++ Tree/BinarysearchTree/BinarySearchTreeNode.java
@@ -0,0 +1,12 @@
+package Tree.BinarySearchTree;
+
+public class BinarySearchTreeNode {
+	int data;
+	BinarySearchTreeNode left;
+	BinarySearchTreeNode right;
+	
+	public BinarySearchTreeNode(int data) {
+		super();
+		this.data = data;
+	}
+}

0314dfb67666bcbf6d550b990f24e8080535beae
Prashant Kumar <2018187@iiitdmj.ac.in>
2025-01-23 19:44:07 +0530

    quick sort

--- Sorting/QuickSort/QuickSortClassTest.java
+++ Sorting/QuickSort/QuickSortClassTest.java
@@ -1,5 +1,9 @@
 package Sorting.QuickSort;
-
+import java.util.*;
 public class QuickSortClassTest {
-    
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,7,9,6};
+        QuickSortClass.quickSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
